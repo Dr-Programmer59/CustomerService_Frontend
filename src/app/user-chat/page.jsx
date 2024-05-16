331d4dab5f50dcdfc11574df7224e9ee08884dde
dr-programmer59 <programmer.abdulaziz@gmail.com>
2024-05-16 11:19:55 +0500

    adding whats app

--- src/app/user-chat/page.jsx
+++ src/app/user-chat/page.jsx
@@ -4,7 +4,8 @@ import React, { useEffect, useState, useRef } from 'react'
 import { IoSend } from "react-icons/io5";
 import axios from 'axios';
 import MessageBox from '@/components/MessageBox';
-
+import { FaWhatsappSquare } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 function page() {
   const questions = ["Hi, I am bot assitant. Hope u have a lovely day Today.", "What's your name sir?(just type name)", "Thanks, Can i know your phone number too?"]
   const [categories, setcategories] = useState([])
@@ -20,16 +21,16 @@ function page() {
   const [imageSrc, setImageSrc] = useState(null);
 
   const [customerDetails, setCustomerDetails] = useState({
-    "name":"unknwon",
-    "phone":"0000"
+    "name": "unknwon",
+    "phone": "0000"
   })
-  
-  
+
+
 
   const [questionindex, setquestionindex] = useState(0);
   const [answers, setanswers] = useState("")
   const audioRef = useRef(null);
-  
+
 
   useEffect(() => {
     const startBotChatting = async () => {
@@ -55,15 +56,15 @@ function page() {
   };
 
   useEffect(() => {
-    
-    if(messages.length == 5){
+
+    if (messages.length == 5) {
       setMessages((prev) => [...prev, { msg: "options", status: "options" }]);
     }
-  },[messages])
+  }, [messages])
 
   const sendQuestions = async (questions) => {
     for (const question of questions) {
-      await setTypeAnimationAndSendMessage(question,0, 2000);
+      await setTypeAnimationAndSendMessage(question, 0, 2000);
     }
   };
 
@@ -72,60 +73,60 @@ function page() {
 
   const startRecording = async () => {
     try {
-      if(recording==false){
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaRecorder.current=new MediaRecorder(stream);
-      
-      const chunks = [];
+      if (recording == false) {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        mediaRecorder.current = new MediaRecorder(stream);
 
-      mediaRecorder.current.ondataavailable = (e) => {
-        chunks.push(e.data);
-      };
+        const chunks = [];
 
-      mediaRecorder.current.onstop = () => {
-        const blob = new Blob(chunks, { type: 'audio/webm' });
-        audioBlob.current=blob;
-      };
+        mediaRecorder.current.ondataavailable = (e) => {
+          chunks.push(e.data);
+        };
 
-      mediaRecorder.current.start();
-      setRecording(true);
-    }
-    else if(recording==true){
-      
-      mediaRecorder.current.stop();
+        mediaRecorder.current.onstop = () => {
+          const blob = new Blob(chunks, { type: 'audio/webm' });
+          audioBlob.current = blob;
+        };
+
+        mediaRecorder.current.start();
+        setRecording(true);
+      }
+      else if (recording == true) {
+
+        mediaRecorder.current.stop();
         setRecording(false);
         socket.emit("send-msg", {
-          message:audioBlob.current,
+          message: audioBlob.current,
           socketId: socket.id,
           category: user.category,
           role: "employee",
           customerSocket: currentCustomer,
-          msgType:"audio"
+          msgType: "audio"
         });
         setRecording(false)
-        setMessages((prev) => [...prev, { msg: audioBlob.current, status: "outgoing",msgType:"audio" }])
-    
-     
-     } // 5 seconds recording time, you can adjust as needed
+        setMessages((prev) => [...prev, { msg: audioBlob.current, status: "outgoing", msgType: "audio" }])
+
+
+      } // 5 seconds recording time, you can adjust as needed
     } catch (error) {
       console.error('Error accessing microphone:', error);
     }
   };
 
 
- 
 
 
 
 
-  
+
+
 
 
 
   const handleReceiveMessage = (data) => {
 
 
-    if (data.msgType== "audio"&& data.message) {
+    if (data.msgType == "audio" && data.message) {
       console.log("there is audio we recive ", data.message)
       const audioBlob = new Blob([data.message], { type: 'audio/webm' });
 
@@ -133,11 +134,11 @@ function page() {
       setMessages((prev) => [...prev, { msg: audioURL, status: "incoming", msgType: "audio" }])
 
     }
-    else if(data.msgType=="img"){
-      setMessages((prev) => [...prev, { msg: data.message, status: "incoming", msgType:"img",imageData:data.imageData }])
+    else if (data.msgType == "img") {
+      setMessages((prev) => [...prev, { msg: data.message, status: "incoming", msgType: "img", imageData: data.imageData }])
     }
-    else{
-      setMessages((prev) => [...prev, { msg: data.message, status: "incoming", msgType: "audio",msgType:"msg"}])
+    else {
+      setMessages((prev) => [...prev, { msg: data.message, status: "incoming", msgType: "audio", msgType: "msg" }])
 
     }
 
@@ -156,7 +157,7 @@ function page() {
         setstartBot(false)
         return
       }
-      
+
       setanswers((prev) => [...prev, message]);
       setMessages((prev) => [...prev, { msg: message, status: "outgoing" }])
       setmessage("")
@@ -165,14 +166,14 @@ function page() {
     }
     else {
       e.preventDefault();
-      if(imageSrc){
-      socket.emit("send-msg", { message, socketId: socket.id, role: "customer",msgType:"img",imageData:imageSrc })
-      setMessages((prev) => [...prev, { msg: message, status: "outgoing",msgType:"img",imageData:imageSrc }])
+      if (imageSrc) {
+        socket.emit("send-msg", { message, socketId: socket.id, role: "customer", msgType: "img", imageData: imageSrc })
+        setMessages((prev) => [...prev, { msg: message, status: "outgoing", msgType: "img", imageData: imageSrc }])
 
       }
-      else{
-        socket.emit("send-msg", { message, socketId: socket.id, role: "customer",msgType:"msg" })
-        setMessages((prev) => [...prev, { msg: message, status: "outgoing",msgType:"msg" }])
+      else {
+        socket.emit("send-msg", { message, socketId: socket.id, role: "customer", msgType: "msg" })
+        setMessages((prev) => [...prev, { msg: message, status: "outgoing", msgType: "msg" }])
 
       }
       setmessage("")
@@ -181,29 +182,31 @@ function page() {
   }
 
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoadingHide(true)
-    }, 3000);
-  },[])
+ 
 
-  
+
   return (
     // this is thediv
     <>
-    {
-      !loadingHide && 
-      <div className='h-screen w-full bg-gradient absolute z-10 flex flex-col justify-center items-center gap-5'>
-        <h2 className='text-white/80 text-3xl'>Hello I am Sundarbhai</h2>
-        <img src='Images/bot.png' className='w-16 h-16 rounded-full'/>
-        <h2 className='text-white/80 text-2xl'>How can i help you ?</h2>
-        <button className='py-2 px-4 rounded-3xl bg-white text-[#330867]' onClick={() => setLoadingHide(true)}>Chat Now !</button>
-      </div>
-    }
-      
+      {
+        !loadingHide &&
+        <div className='h-screen w-full bg-gradient absolute z-10 flex flex-col justify-center items-center gap-5'>
+          <h2 className='text-white/80 text-3xl'>Hello I am Sundarbhai</h2>
+          <img src='Images/bot.png' className='w-16 h-16 rounded-full' />
+          
+          <div class="relative mb-6">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+             <FaWhatsapp className='w-5 h-5'/>
+            </div>
+            <input type="text" id="input-group-1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your what's app"/>
+          </div>
+          <button className='py-2 px-4 rounded-3xl bg-white text-[#330867]' onClick={() => setLoadingHide(true)}>Chat Now !</button>
+        </div>
+      }
+
       <div className='gradint p-3 rounded-t-md flex justify-between items-center shadow-md shadow-[#330867]'>
         <div className='flex gap-2 items-center'>
-          <img src='Images/bot.png' className='w-16 h-16 rounded-full'/>
+          <img src='Images/bot.png' className='w-16 h-16 rounded-full' />
           <div className=''>
             <h1 className='text-white text-2xl'>Sundarbhai</h1>
             <p className='text-white/80'>we are online</p>
@@ -211,11 +214,11 @@ function page() {
         </div>
       </div>
       <div class="  shadow-lg  bg-white p-4">
-  
-  
-        <MessageBox messages={messages}  typingAnimation={typingAnimation} setMessages={setMessages}  message={message} setmessage={setmessage} handleSendMessage={handleSendMessage} audioBlob={audioBlob}  startRecording={startRecording} sendQuestions={sendQuestions} socket={socket} answers={answers} imageSrc={imageSrc} setImageSrc={setImageSrc} startBot={startBot} setstartBot={setstartBot} />
-        
-  
+
+
+        <MessageBox messages={messages} typingAnimation={typingAnimation} setMessages={setMessages} message={message} setmessage={setmessage} handleSendMessage={handleSendMessage} audioBlob={audioBlob} startRecording={startRecording} sendQuestions={sendQuestions} socket={socket} answers={answers} imageSrc={imageSrc} setImageSrc={setImageSrc} startBot={startBot} setstartBot={setstartBot} />
+
+
       </div>
     </>
 
